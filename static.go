81923dcf39ab9493707c0875915673dbb2701861
Daniel Krech <eikeon@eikeon.com>
2013-05-28 01:34:02 -0400

    Added setting of Cache-Control header back.

--- static.go
+++ static.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"path"
+	"time"
 )
 
 func StaticHandler(w http.ResponseWriter, req *http.Request) {
+	now := time.Now()
+	d := time.Date(2011, 4, 11, 0, 0, 0, 0, time.Local)
+	TTL := int64(86400)
+	ttl := TTL - (now.Unix()-d.Unix())%TTL // shift
+	w.Header().Set("Cache-Control", fmt.Sprintf("max-age=%d", ttl))
+
 	http.ServeFile(w, req, path.Join(*StaticRoot, req.URL.Path))
 }
