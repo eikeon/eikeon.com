1a0a6bc4bc31d84902b3ec06fc8386c768f29b5c
Daniel Krech <eikeon@eikeon.com>
2013-09-22 22:48:42 -0400

    Updated.

---
+++ Gruntfile.js
@@ -0,0 +1,137 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bower: grunt.file.readJSON('bower.json'),
+        banner: '/**\n' +
+                '* <%= bower.name %>.js v<%= bower.version %> \n' +
+                '* <%= grunt.template.today("yyyy/mm/dd") %> \n' +
+                '*/\n',
+	shell: {
+            goinstall: {
+                options: {
+                    failOnError: true,
+                    stdout: true,
+                    execOptions: {
+			cwd: '.'
+                    }
+		},
+		command: 'go install -v ./...'
+            }
+	},
+        clean: {
+            static: ['static/<%= bower.name %>/', 'build']
+        },
+        ngmin: {
+            eikeon: {
+                src: ['js/<%= bower.name %>.js'],
+                dest: 'build/js/<%= bower.name %>.annotate.js'
+            },
+        },
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: false
+            },
+            eikeon: {
+                src: ['bower_components/jquery/jquery.min.js', 'bower_components/bootstrap/dist/js/bootstrap.min.js'],
+                dest: 'static/<%= bower.version %>/js/<%= bower.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            eikeon: {
+                files: {
+                    'static/<%= bower.version %>/js/<%= bower.name %>.min.js': ['<%= concat.eikeon.dest %>']
+                }
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: 'js/.jshintrc'
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            src: {
+                src: ['js/*.js']
+            },
+            test: {
+                src: ['js/tests/unit/*.js']
+            }
+        },
+        recess: {
+            options: {
+                compile: true
+            },
+            eikeon: {
+                files: {
+                    'static/<%= bower.version %>/css/<%= bower.name %>.css': ['less/eikeon.less']
+                }
+            },
+            min: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    'static/<%= bower.version %>/css/<%= bower.name %>.min.css': ['less/eikeon.less']
+                }
+            }
+        },
+        typescript: {
+            base: {
+                src: ['bower_components/hue-color-converter/colorconverter.ts'],
+                dest: 'build/js/colorconverter.js'
+            }
+        },
+        copy: {
+            images: {
+                files: [
+                    {
+                        src: 'images/*',
+                        dest: 'static/<%= bower.version %>/'
+                    },
+                    {
+			expand: true,
+			cwd: 'bower_components/bootstrap/dist/',
+                        src: ['fonts/*'],
+                        dest: 'static/<%= bower.version %>/'
+                    }
+                ]
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-ngmin');
+    grunt.loadNpmTasks('grunt-recess');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Test task.
+    grunt.registerTask('test', ['jshint']);
+
+    // JS distribution task.
+    grunt.registerTask('static-js', ['typescript', 'ngmin', 'concat', 'uglify']); 
+
+    // CSS distribution task.
+    grunt.registerTask('static-css', ['recess']);
+
+    // Images distribution task
+    grunt.registerTask('static-images', ['copy']);
+
+    // Full distribution task.
+    grunt.registerTask('static', ['clean', 'static-css', 'static-js', 'static-images']);
+
+    // Default task.
+    grunt.registerTask('default', ['shell', 'test', 'static']);
+
+};
--- web.go
+++ web.go
@@ -3,129 +3,149 @@ package main
 import (
 	"bytes"
 	"crypto/md5"
+	"encoding/json"
 	"fmt"
+	"go/build"
 	"html/template"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"path"
 )
 
-var site = template.Must(template.ParseFiles("templates/site.html"))
+var pkg struct {
+	Version string `json:"version"`
+}
+var Root string
+var site *template.Template
+var templates = make(map[string]*template.Template)
 
-func makeTemplate(names ...string) *template.Template {
-	t, err := site.Clone()
-	if err != nil {
-		log.Fatal("cloning site: ", err)
+func init() {
+	if p, err := build.Default.Import("github.com/eikeon/eikeon.com", "", build.FindOnly); err == nil {
+		Root = p.Dir
+	} else {
+		log.Println("WARNING: could not import package:", err)
 	}
-	return template.Must(t.ParseFiles(names...))
-}
 
-type View interface {
-	Prefix() string
-	Name() string
-	Match(req *http.Request) bool
-	Data(req *http.Request) Data
-}
-
-type Data map[string]interface{}
-
-type view struct {
-	prefix, name string
-	data         Data
-}
-
-func (v *view) Prefix() string {
-	return v.prefix
-}
-
-func (v *view) Name() string {
-	return v.name
-}
-
-func (v *view) Match(req *http.Request) bool {
-	return req.URL.Path == v.Prefix()
-}
-
-func (v *view) Data(req *http.Request) Data {
-	if v.data == nil {
-		v.data = make(Data)
+	if j, err := os.OpenFile(path.Join(Root, "bower.json"), os.O_RDONLY, 0666); err == nil {
+		dec := json.NewDecoder(j)
+		if err = dec.Decode(&pkg); err != nil {
+			log.Println("WARNING: could not decode bower.json", err)
+		}
+		j.Close()
+	} else {
+		log.Println("WARNING: could not open bower.json", err)
 	}
-	v.data["Title"] = v.Name()
-	return v.data
+
 }
 
-func add(view View) {
-	t := makeTemplate("templates/" + view.Name() + ".html")
-	http.HandleFunc(view.Prefix(), func(w http.ResponseWriter, req *http.Request) {
-		var d Data
-		if view.Match(req) {
-			d = view.Data(req)
-		} else {
-			//w.Header().Set("Cache-Control", "max-age=10, must-revalidate")
-			//w.WriteHeader(http.StatusNotFound)
-			StaticHandler(w, req)
-			return
+type longExpireHandler struct {
+	h http.Handler
+}
+
+func longExpire(h http.Handler) http.Handler {
+	return &longExpireHandler{h}
+}
+
+func (le *longExpireHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ttl := int64(86400)
+	w.Header().Set("Cache-Control", fmt.Sprintf("max-age=%d", ttl))
+	le.h.ServeHTTP(w, r)
+}
+
+func getTemplate(name string) *template.Template {
+	if t, ok := templates[name]; ok {
+		return t
+	} else {
+		if site == nil {
+			site = template.Must(template.ParseFiles(path.Join(Root, "templates/site.html")))
 		}
-		var bw bytes.Buffer
-		h := md5.New()
-		mw := io.MultiWriter(&bw, h)
-		err := t.ExecuteTemplate(mw, "html", d)
-		if err == nil {
-			w.Header().Set("ETag", fmt.Sprintf(`"%x"`, h.Sum(nil)))
-			w.Header().Set("Content-Length", fmt.Sprintf("%d", bw.Len()))
-			w.Write(bw.Bytes())
-		} else {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+		t, err := site.Clone()
+		if err != nil {
+			log.Fatal("cloning site: ", err)
 		}
+		t = template.Must(t.ParseFiles(path.Join(Root, name)))
+		templates[name] = t
+		return t
+	}
+}
+
+type templateData map[string]interface{}
+
+func writeTemplate(t *template.Template, d templateData, w http.ResponseWriter) {
+	var bw bytes.Buffer
+	h := md5.New()
+	mw := io.MultiWriter(&bw, h)
+	err := t.ExecuteTemplate(mw, "html", d)
+	if err == nil {
+		w.Header().Set("ETag", fmt.Sprintf(`"%x"`, h.Sum(nil)))
+		w.Header().Set("Content-Length", fmt.Sprintf("%d", bw.Len()))
+		w.Write(bw.Bytes())
+	} else {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handleTemplate(prefix, name string, data templateData) {
+	t := getTemplate("templates/" + name + ".html")
+	http.HandleFunc(prefix, func(w http.ResponseWriter, req *http.Request) {
+		d := templateData{}
+		d["Title"] = name
+		d["Version"] = pkg.Version
+		if data != nil {
+			for k, v := range data {
+				d[k] = v
+			}
+		}
+		if req.URL.Path == prefix {
+			d["Found"] = true
+		} else {
+			w.Header().Set("Cache-Control", "max-age=10, must-revalidate")
+			w.WriteHeader(http.StatusNotFound)
+		}
+		writeTemplate(t, d, w)
 	})
 }
 
 func ListenAndServe(address string) {
-	add(&view{prefix: "/", name: "home", data: Data{"Recipes": Recipes}})
-	add(&view{prefix: "/resume/", name: "resume", data: Data{}})
-	add(&view{prefix: "/recipes/", name: "recipes", data: Data{"Recipes": Recipes}})
+	fs := longExpire(http.FileServer(http.Dir(path.Join(Root, "static/"))))
+	http.Handle("/"+pkg.Version+"/", fs)
 
-	t := makeTemplate("templates/" + "recipe" + ".html")
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/" {
+			d := templateData{"Version": pkg.Version, "Recipes": Recipes}
+			t := getTemplate("templates/" + "home" + ".html")
+			writeTemplate(t, d, w)
+		} else {
+			fs.ServeHTTP(w, req)
+			return
+		}
+	})
+
+	handleTemplate("/resume/", "resume", templateData{})
+	handleTemplate("/recipes/", "recipes", templateData{"Recipes": Recipes})
+
+	t := getTemplate("templates/" + "recipe" + ".html")
 	http.HandleFunc("/recipe/", func(w http.ResponseWriter, req *http.Request) {
-
+		d := templateData{"Version": pkg.Version}
 		var r = Recipes[path.Base(req.URL.Path)]
 		if r == nil {
+			d["Found"] = false
 			w.Header().Set("Cache-Control", "max-age=10, must-revalidate")
 			w.WriteHeader(http.StatusNotFound)
 			//return
 		} else {
+			d["Recipe"] = r
+			d["Found"] = true
 			if req.URL.Path[len(req.URL.Path)-1] != '/' {
 				http.Redirect(w, req, req.URL.Path+"/", http.StatusMovedPermanently)
 				return
 			}
 		}
-		// setCacheControl(w, req)
-		// page := newPage(r.Name + " Recipe")
-		// page.Recipe = r
-		// page.Write(w, req)
-
-		var d Data = Data{"Recipe": r}
-		// if view.Match(req) {
-		// 	d = view.Data(req)
-		// } else {
-		// }
-		var bw bytes.Buffer
-		h := md5.New()
-		mw := io.MultiWriter(&bw, h)
-		err := t.ExecuteTemplate(mw, "html", d)
-		if err == nil {
-			w.Header().Set("ETag", fmt.Sprintf(`"%x"`, h.Sum(nil)))
-			w.Header().Set("Content-Length", fmt.Sprintf("%d", bw.Len()))
-			w.Write(bw.Bytes())
-		} else {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
+		writeTemplate(t, d, w)
 	})
 
-	// add(&view{prefix: "/transition/", name: "transition", data: Data{"Recipes": Recipes}})
-	http.HandleFunc("/bootstrap/", StaticHandler)
-	http.HandleFunc("/jquery/", StaticHandler)
 	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Print("ListenAndServe:", err)
