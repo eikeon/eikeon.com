07e9ea5c927f43980a3c1116557d54a6a9a31a15
Daniel Krech <eikeon@eikeon.com>
2015-01-08 11:24:54 -0500

    Switched to vinyl-transform; introduced autoprefix

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,51 +4,47 @@
   var gulp       = require('gulp'),
       browserify = require('browserify'),
       reactify = require('reactify'),
-      source = require('vinyl-source-stream'),
       buffer = require('vinyl-buffer'),
+      transform = require('vinyl-transform'),
       uglify = require('gulp-uglify'),
       sourcemaps = require('gulp-sourcemaps'),
       concat     = require('gulp-concat'),
       imagemin   = require('gulp-imagemin'),
+      autoprefixer   = require('gulp-autoprefixer'),
       connect = require('connect'),
       http = require('http'),
       handler = require('./server'),
       less = require('gulp-less'),
       path = require('path');
 
-  gulp.task('styles', function () {
-    gulp.src(['assets/css/styles.css'])
-      .pipe(concat('styles.css'))
-      .pipe(gulp.dest('./static/'));
-  });
-
   gulp.task('scripts', function() {
 
-    var bundler = browserify({
-      entries: ['./assets/js/site.js'],
-      debug: true
+    var browserified = transform(function(filename) {
+      var b = browserify({
+        entries: [filename],
+        debug: true
+      });
+      return b.bundle();
     });
 
-    var bundle = function() {
-      return bundler
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-      // Add transformation tasks to the pipeline here.
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./static/js/'));
-    };
-
-    return bundle();
+    return gulp.src(['./assets/js/site.js'])
+      .pipe(browserified)
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(uglify())
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./static/js'));
   });
 
   gulp.task('less', function () {
     gulp.src('assets/less/*.less')
+      .pipe(sourcemaps.init())
       .pipe(less({
         paths: [ path.join(__dirname, 'less', 'includes') ]
       }))
+      .pipe(autoprefixer())
+      .pipe(concat('site.css'))
+      .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('./static/css'));
   });
 
@@ -72,11 +68,11 @@
 
   gulp.task('watch', function() {
     gulp.watch('assets/js/**/*.js', [ 'scripts' ]);
-    gulp.watch('assets/css/**/*.css', [ 'styles' ]);
+    gulp.watch('assets/less/*.less', [ 'less' ]);
     gulp.watch('assets/images/**/*', [ 'images' ]);
   });
   
-  gulp.task('static', ['styles', 'less', 'scripts', 'images', 'bootstrap', 'ubuntu' ]);
+  gulp.task('static', ['less', 'scripts', 'images', 'bootstrap', 'ubuntu' ]);
 
   gulp.task('webserver', function() {
     var app = connect()
